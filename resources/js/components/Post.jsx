15cb1b15e22180d516061a5bcbd3099e3c1f37ae
Francis Oke <francisokewale@gmail.com>
2022-03-29 19:20:44 +0100

    make post cards the same height

--- resources/js/components/Post.jsx
+++ resources/js/components/Post.jsx
@@ -7,9 +7,7 @@ export default function Post(props) {
     return(
         <Link href={post.url}>
             <div className="rounded-sm overflow-hidden shadow shadow-stone-200 hover:shadow-lg hover:scale-105 ease-in-out duration-300">
-                <div>
-                    <img src={post.imagelink} alt={`image for ${post.title}`} className="w-100" />
-                </div>
+                <div className="h-40 w-full bg-center bg-cover bg-no-repeat" style={{backgroundImage: `url(${post.imagelink})`}} role="image" alt={post.title}></div>
                 <div className="p-6">
                     <p className="font-bold truncate ...">{post.title}</p>
                     <p className="text-xs text-gray-400 text-right mt-3">{timestampToDate(post.created_at)}</p>
